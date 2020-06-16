816dc51fb56933db54edaca87164449915866637
Andrew Holway <andrew.holway@gmail.com>
2020-06-16 16:07:35 +0200

    new api

---
+++ api.py
@@ -0,0 +1,33 @@
+import csv
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def import_data(file_name):
+    return_list = []
+    with open(file_name) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+                return_list.append(row)
+    return return_list
+    
+@app.route('/')
+def root_data():
+    data = import_data('uk-towns-sample.csv')
+    return jsonify(data)
+
+@app.route('/county/<search_term>')
+def by_county(search_term):
+    data = import_data('uk-towns-sample.csv')
+    return_list = []
+    for row in data:
+        if row["county"] == search_term:
+            return_list.append(row)
+    return jsonify(return_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
--- sophie.py
+++ sophie.py
@@ -1,6 +1,6 @@
-import csv
 
-with open('/Users/sophie/Desktop/uk-towns/uk-towns-sample.csv') as csvfile:    
+
+with open('/Users/andrew/dev/uk-towns/uk-towns-sample.csv') as csvfile:    
     reader = csv.DictReader(csvfile)
     for row in reader:
         if row["county"] == "Warwickshire":
