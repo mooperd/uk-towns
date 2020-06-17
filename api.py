da3ae4f4f27e541a0026054c594e7ecbfa69777c
sophella <66617862+sophella@users.noreply.github.com>
2020-06-17 15:35:47 +0100

    added search by lat and elevation

--- api.py
+++ api.py
@@ -25,6 +25,15 @@ def by_county(search_term):
             return_list.append(row)
     return jsonify(return_list)
 
+@app.route('/latel/<search_term>')
+def by_latel(search_term):
+    data = import_data('uk-towns-sample.csv')
+    return_list = []
+    for row in data:
+        if row["latitude"] >= search_term and row["elevation"] >= search_term:
+            return_list.append(row)
+    return jsonify(return_list)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
