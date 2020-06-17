d2fb2877c27979a9a05ed0f9cfc8796a80eb3629
Meister200315 <66568245+Meister200315@users.noreply.github.com>
2020-06-17 17:58:25 +0200

    Update api.py
    endpoint country in new branch created

--- api.py
+++ api.py
@@ -25,6 +25,24 @@ def by_county(search_term):
             return_list.append(row)
     return jsonify(return_list)
 
+@app.route('/name/<search_term>')
+def by_name(search_term):
+    data = import_data('uk-towns-sample.csv')
+    return_list = []
+    for row in data:
+        if row["name"] == search_term:
+            return_list.append(row)
+    return jsonify(return_list)
+
+@app.route('/country/<search_term>')
+def by_country(search_term):
+    data = import_data('uk-towns-sample.csv')
+    return_list = []
+    for row in data:
+        if row["country"] == search_term:
+            return_list.append(row)
+    return jsonify(return_list)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
